777cca3b5aee725fbca829aba800988eaf287300
miropg <miro.npg28@gmail.com>
2025-05-31 12:27:48 -0700

    1 passenger test

--- Project_3/part1/park.c
+++ Project_3/part1/park.c
@@ -133,7 +133,7 @@ void board(Passenger* p) {
         printf("Passenger %d boarded Car %d\n", p->pass_id, my_car->car_id);
         pthread_mutex_unlock(&print_lock);
         my_car->passenger_ids[my_car->onboard_count++] = p->pass_id;
-
+        pthread_cond_signal(&passengers_waiting);
         // if (my_car->onboard_count == my_car->capacity) {
         //     pthread_cond_signal(&all_boarded);
         // }
