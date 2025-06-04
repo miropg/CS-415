d90c470f5f44fd4d934245d5427602b6bc9f37af
miropg <miro.npg28@gmail.com>
2025-06-03 22:30:16 -0700

    working main

--- Project_3/part3/monitor.c
+++ Project_3/part3/monitor.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
-#include <pthread.h>
-#include "monitor.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <time.h>
+// #include <string.h>
+// #include <pthread.h>
+// #include "monitor.h"
 
-//int pipe_fd is the read end of a UNIX pipe
-// In monitor_main, replace its loop with something like:
-void monitor_main(int pipe_fd) {
-    setvbuf(stdout, NULL, _IOLBF, 0);
-    FILE *pipe_stream = fdopen(pipe_fd, "r");
-    if (!pipe_stream) {
-        perror("monitor_main: fdopen");
-        return;
-    }
+// //int pipe_fd is the read end of a UNIX pipe
+// // In monitor_main, replace its loop with something like:
+// void monitor_main(int pipe_fd) {
+//     setvbuf(stdout, NULL, _IOLBF, 0);
+//     FILE *pipe_stream = fdopen(pipe_fd, "r");
+//     if (!pipe_stream) {
+//         perror("monitor_main: fdopen");
+//         return;
+//     }
     
-    char bigbuf[4096];
-    while (fgets(bigbuf, sizeof(bigbuf), pipe_stream)) {
-        //pause();
-        //calic
-        pthread_mutex_lock(shared_print_mutex);
-        fputs(bigbuf, stdout);
-        fflush(stdout);
-        pthread_mutex_unlock(shared_print_mutex);
-    }
-    fclose(pipe_stream);
-}
+//     char bigbuf[4096];
+//     while (fgets(bigbuf, sizeof(bigbuf), pipe_stream)) {
+//         //pause();
+//         //calic
+//         pthread_mutex_lock(shared_print_mutex);
+//         fputs(bigbuf, stdout);
+//         fflush(stdout);
+//         pthread_mutex_unlock(shared_print_mutex);
+//     }
+//     fclose(pipe_stream);
+// }
 
